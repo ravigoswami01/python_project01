88b0c22505154e16836bf7f0227658080a3d429d
Ravi Goswami <raviroa25@gmail.com>
2026-06-02 20:49:21 +0530

    add initial implementation of arithmetic and temperature conversion functions

---
+++ opperter.py
@@ -0,0 +1,21 @@
+def addTownum(num1, num2):
+    return num1 + num2
+
+result = addTownum(2344, 635632)
+print(result)
+def user():
+    print("Hello, welcome to the program!")
+
+
+user()
+
+def temparture(celsius):
+    farahite = (celsius * 9/5) + 32
+    return farahite
+
+temp= temparture(25)
+print(temp)
+
+def free():
+    pass
+print("This is a free function that does not take any parameters.")
