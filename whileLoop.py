b851977201283fc0d36c4860abbb5511a7c61c28
Ravi Goswami <raviroa25@gmail.com>
2026-06-05 07:28:18 +0530

    refactor while loop section and add nested loop for printing stars

--- whileLoop.py
+++ whileLoop.py
@@ -1,8 +1,19 @@
 # print 1 to 10 number and multiple by 2
 
-count = 1
+# count = 1
 
-while count < 10 :
-    print(count)
-    count += 1
+# while count < 10 :
+#     print(count)
+#     count += 1
 
+
+
+
+# new nested loop 
+
+n = 5
+
+for i in range(1,n+1):
+    for j in range(1 ,i+1):
+        print("*" , end=" ")
+    print()
