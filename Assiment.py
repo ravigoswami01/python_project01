e00809be76901ee4668646ba27244029b20a120a
Ravi Goswami <raviroa25@gmail.com>
2026-06-05 09:37:21 +0530

    add pyramid pattern printing functionality

---
+++ Assiment.py
@@ -0,0 +1,35 @@
+# # print("this is an apple", sep="*",end="-")
+# # print("Ravi")
+
+# # i = 1
+# # while i <4:
+# #     print(f"hellow world{i}",sep="  ", end="*")
+# #     i += 1
+
+# # i = 1
+# # while i <=10:
+# #     print(i * 2)
+# #   i += 1
+n = 5
+# # for i in range(1, n+1):
+# #     for j in range(1 , i+1):
+# #         print("*", end="")
+# #     print()
+
+
+for i in range(n , 0,-1):
+    for j in range(1 , i +1):
+         print("*", end=" ")
+    print()
+
+# # for shourtcut formate 
+for i in range(1 , n+1):
+     print("*" * i)
+
+for i in range(n , 0 , -1):
+     print("* " *  i)
+ 
+
+for i in range(1 , n+1):
+    print(" " * (n -i),end="")
+    print("*" * (2 * i -1))
