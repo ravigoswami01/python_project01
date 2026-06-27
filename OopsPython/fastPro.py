7e6c8c193b4dfe230530cda16fab7771887ba892
Ravi Goswami <raviroa25@gmail.com>
2026-06-27 21:52:08 +0530

    Add initial implementation of lambda functions for addition and even check

---
+++ OopsPython/fastPro.py
@@ -0,0 +1,18 @@
+
+# lamda expression for function
+
+
+# add tow number 
+
+
+chh = lambda a , b : a + b
+
+
+print("this is " , chh(23 , 44))
+
+
+# checck number is even if even return true / false
+
+is_even = lambda a: a % 2 == 0
+
+print(is_even(59))
